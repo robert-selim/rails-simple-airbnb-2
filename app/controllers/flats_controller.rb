08e9387eab9e9d558061522f51054fbea7741864
Robert Hill <hill.rws@gmail.com>
2020-08-12 08:23:38 +0200

    js and stylesheets

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,7 +1,12 @@
 class FlatsController < ApplicationController
 
   def index
-    @flats = Flat.all
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?", "%#{params[:query]}%")
+    else
+      @flats = Flat.all
+    end
   end
 
 end
